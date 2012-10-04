73bfc17eebd11156d51f5d9122286f797b976192
Josuah Aron <josuah@josuah-K43SJ.(none)>
2012-10-04 10:42:24 +0200

    added abstract class as public interface (working with contributors :p )

---
+++ inc/AbstractGraph.hpp
@@ -0,0 +1,38 @@
+#ifndef ABSTRACTGRAPH
+#define ABSTRACTGRAPH
+
+#include <set>
+#include <map>
+#include "Node.hpp"
+
+class AbstractGraph{
+public:
+  // AbstractGraph();
+  // AbstractGraph(const AbstractGraph & source);
+  // ~AbstractGraph();
+
+  // AbstractGraph & operator=(const AbstractGraph & source);
+
+  // Prédicats
+  virtual bool is_empty()=0;
+  virtual bool has_node(Node::node_id id)=0;
+  virtual bool has_edge(Node::node_id id1, Node::node_id id2)=0;
+
+  //Constructeurs
+  virtual void add_node(Node::node_id id)=0;
+  virtual void remove_node(Node::node_id id)=0;
+  virtual void add_edge(Node::node_id id1, Node::node_id id2)=0;
+  virtual void remove_edge(Node::node_id id1, Node::node_id id2)=0;
+
+  //observateurs
+  virtual unsigned long size()const=0;
+  virtual std::set<Node::node_id> successors(Node::node_id node)=0;
+  virtual Node::node_id first_node()=0;
+  virtual Node::node_id next_node()=0;
+  virtual Node::node_id previous_node()=0;
+  virtual Node::node_id last_node()=0;
+  virtual unsigned long internally(Node::node_id node)=0;
+  virtual unsigned long externally(Node::node_id node)=0;
+};
+
+#endif
---
+++ src/AbstractGraph.cpp
@@ -0,0 +1,88 @@
+#include "AbstractGraph.hpp"
+
+// Graph0::Graph0():_it(_nodes.begin()){}
+
+// Graph0::Graph0(const Graph0 & source):_nodes(source._nodes),_topology(source._topology), _it(_nodes.begin()){
+// }
+
+// Graph0::~Graph0(){
+// }
+
+// Graph0 & Graph0::operator=(const Graph0 & source){
+//   _nodes = source._nodes;
+//   _topology = source._topology;
+//   _it = _nodes.begin();
+//   return *this;
+// }
+
+bool AbstractGraph::is_empty(){
+ std::cout << "calling abstract AbstractGraph::is_empty()" << std::endl;
+  return false;
+}
+
+bool AbstractGraph::has_node(Node::node_id id){
+  std::cout << "calling abstract AbstractGraph::has_node()" << std::endl;
+  
+  return false;
+}
+
+bool AbstractGraph::has_edge(Node::node_id id1, Node::node_id id2){
+  std::cout << "calling abstract AbstractGraph::has_edge()" << std::endl;
+  return false;
+}
+
+void AbstractGraph::add_node(Node::node_id id){ 
+  std::cout << "calling abstract AbstractGraph::add_node()" << std::endl;
+}
+
+void AbstractGraph::remove_node(Node::node_id id){
+  std::cout << "calling abstract AbstractGraph::remove_node()" << std::endl;
+}
+
+void AbstractGraph::add_edge(Node::node_id id1, Node::node_id id2){
+  std::cout << "calling abstract AbstractGraph::add_edge()" << std::endl;
+}
+
+void AbstractGraph::remove_edge(Node::node_id id1, Node::node_id id2){ 
+  std::cout << "calling abstract AbstractGraph::remove_edge()" << std::endl;
+}
+
+unsigned long AbstractGraph::size()const{
+  std::cout << "calling abstract AbstractGraph::size()" << std::endl;
+  return 0;
+}
+
+std::set<Node::node_id> AbstractGraph::successors(Node::node_id node){
+  std::cout << "calling abstract AbstractGraph::successors()" << std::endl;
+  return std::set<Node::node_id>() ;
+}
+
+Node::node_id AbstractGraph::first_node(){
+  std::cout << "calling abstract AbstractGraph::first_node()" << std::endl;
+  return 0;
+}
+
+Node::node_id AbstractGraph::next_node(){
+  std::cout << "calling abstract AbstractGraph::next_node()" << std::endl;
+  return 0;
+}
+
+Node::node_id AbstractGraph::previous_node(){ 
+  std::cout << "calling abstract AbstractGraph::previous_node()" << std::endl;
+  return 0;
+}
+
+Node::node_id AbstractGraph::last_node(){
+  std::cout << "calling abstract AbstractGraph::last_node()" << std::endl;
+  return 0;
+}
+
+unsigned long internally(Node::node_id node){
+  std::cout << "calling abstract AbstractGraph::internally()" << std::endl;
+  return 0;
+}
+
+unsigned long externally(Node::node_id node){
+  std::cout << "calling abstract AbstractGraph::externally()" << std::endl;
+  return 0;
+}
